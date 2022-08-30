521d49ec72755a1e23d8146481ae877437d11d0d
rizsyad <rizsyad@gmail.com>
2022-08-30 12:58:35 +0700

    add leakdata and uploaded heroku

--- helpers/validation.js
+++ helpers/validation.js
@@ -7,6 +7,13 @@ const isValidVerify = (text) => {
   return validate;
 };
 
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
 module.exports = {
   isValidVerify,
+  validateEmail,
 };
