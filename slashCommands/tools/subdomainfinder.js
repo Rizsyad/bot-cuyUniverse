02a04c4550188187c33f5f76671ec4bb9e12710e
rizsyad <rizsyad@gmail.com>
2022-08-31 17:08:50 +0700

    update 2 tools

---
+++ slashCommands/tools/subdomainfinder.js
@@ -0,0 +1,38 @@
+const { sendFileText, errorEmbed } = require("../../helpers/utility");
+const { getExtraxtPageLinks } = require("../../helpers/requests");
+const { isValidURL } = require("../../helpers/validation");
+const Subfinder = require("@sooluh/subfinder");
+const subfinder = new Subfinder();
+
+module.exports = {
+  name: "subdomainfinder",
+  description: "find as many subdomains as possible",
+  category: "tools",
+  options: [
+    {
+      name: "domain",
+      description: "Input a domain",
+      type: "STRING",
+      required: true,
+    },
+  ],
+  run: async (client, interaction, args) => {
+    let domain = interaction.options.getString("domain");
+
+    if (!isValidURL(domain)) {
+      return interaction.followUp({ embeds: [errorEmbed(`invalid domain`)] });
+    }
+
+    domain = domain
+      .replace(/^https?:\/\//, "")
+      .replace("www.", "")
+      .replace("/", "");
+
+    let data = await subfinder.lookup(domain);
+    data = data.map((datas) => `${datas.subdomain} (${datas.status})`);
+    const file = sendFileText(data.join("\n") || "None");
+
+    const outputMessage = { files: [file] };
+    await interaction.followUp(outputMessage);
+  },
+};
