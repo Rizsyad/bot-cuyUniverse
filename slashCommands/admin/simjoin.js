7c9271be310ab7e8f5e6dc1abcfdef1f70b4b0ac
zynxyz <nazihanf132@gmail.com>
2022-09-01 21:42:29 +0700

    Add validation

--- slashCommands/admin/simjoin.js
+++ slashCommands/admin/simjoin.js
@@ -1,10 +1,27 @@
+const { embeed, errorEmbed, successEmbed } = require("../../helpers/utility");
+const { findGuild, createLogs, updateLogs } = require("../../database/models/guildLogUsersModel");
+
 module.exports = {
   name: "simjoin",
   description: "Simulator user join server",
   category: "admin",
   userPermissions: ["ADMINISTRATOR"],
   run: async (client, interaction, args) => {
-    interaction.followUp({ content: "simulation join member" });
-    client.emit("guildMemberAdd", interaction.member);
+    const getGuild = await findGuild(interaction.guild.id);
+
+    if (getGuild == null || !getGuild.activeWelcomMessage) {
+      return interaction.followUp({
+        embeds: [
+          errorEmbed(
+            "Your Welcome Message is disable, please enable first before use this commands."
+          ),
+        ],
+      });
+    }
+
+    if (getGuild.activeWelcomMessage) {
+      interaction.followUp({ content: "Simulation joinmember" });
+      client.emit("guildMemberAdd", interaction.member);
+    }
   },
 };
--- slashCommands/admin/welcomemessage.js
+++ slashCommands/admin/welcomemessage.js
@@ -51,7 +51,7 @@ module.exports = {
 
       const welcomeMessage = getMessage.content;
 
-      if (getGuild.activeWelcomMessage) {
+      if (getGuild?.activeWelcomMessage) {
         return interaction.followUp({
           content: "Your Welcome Message is already enable",
         });
