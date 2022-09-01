3e9c1abbd3e82f1e61016d5237d77403bc614344
rizsyad <rizsyad@gmail.com>
2022-09-01 15:40:02 +0700

    slicing and add message leave

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { DB_USER, DB_PASS, DB_HOST, DEV } = require("../config");
 const conn = DEV
   ? "mongodb://127.0.0.1:27017/cuybot"
-  : `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/cuybot?retryWrites=true&w=majority`;
+  : `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/cuybot`;
 
 mongoose.connect(conn).then(() => {
   console.log("Connected to mongodb");
